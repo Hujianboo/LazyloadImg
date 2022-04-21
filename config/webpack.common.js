62be6f161a40a6f880d6332b5e8933807b7bdeed
Hujianboo <466636709@qq.com>
2022-04-21 19:33:07 +0800

    feat:add

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -6,7 +6,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.tsx",
   output: {
-    path: path.resolve(__dirname, "../dist"),
+    path: path.resolve(__dirname, "../docs"),
     filename: "[name].[contenthash].js",
     publicPath: "",
   },
